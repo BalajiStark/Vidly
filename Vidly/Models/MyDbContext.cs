13715f8910fc2a1f07bfe1640faa33e618055024
Balaji <balajicse.2910@gmail.com>
2020-09-25 22:57:57 +0530

    Add entityframe work and Dbcontext

---
+++ Vidly/Models/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Customers> customers { get; set; }
+        public MyDbContext()
+        {
+        }
+    }
+}
