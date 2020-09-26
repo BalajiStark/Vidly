84c4786e340a3b24416ad36b9b1c6aba06ac65bf
Balaji <balajicse.2910@gmail.com>
2020-09-26 23:34:16 +0530

    Editing a customer form and saving in Db

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -6,6 +6,7 @@ using System.Data.Entity;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Runtime.Remoting.Messaging;
 
 namespace Vidly.Controllers
 {
@@ -19,7 +20,7 @@ namespace Vidly.Controllers
             _context = new MyDbContext();
         }
 
-        public ActionResult New()
+        public ActionResult CustomerForm()
         {
             var memberShipTypes = _context.memberShipTypes.ToList();
             var viewmodel = new CustomerFormViewModel
@@ -28,12 +29,47 @@ namespace Vidly.Controllers
             };
             return View(viewmodel);
         }
+
+        [HttpPost]
+        public ActionResult Create(CustomerFormViewModel customerDetails)
+        {
+            var customer = customerDetails.Customers;
+            if (customer.Id == 0)
+            {
+                _context.customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.customers.Include(c => c.MemberShipType).Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.DateofBirth = customer.DateofBirth;
+                customerInDb.MemberShipTypeId = customer.MemberShipTypeId;
+                customerInDb.IsSubscribed = customer.IsSubscribed;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("CustomersList", "Customers");
+        }
         public ActionResult CustomersList()
         {
             var customers = _context.customers.Include(c => c.MemberShipType).ToList();
             return View(customers);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.customers.Include(c => c.MemberShipType).Single(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewmodel = new CustomerFormViewModel
+            {
+                Customers = customer,
+                MemberShipType = _context.memberShipTypes.ToList()
+            };
+            return View("CustomerForm" , viewmodel);
+        }
         public ActionResult CustomerDetails(int id)
         {
             var customer = _context.customers.Include(c => c.MemberShipType).SingleOrDefault(c => c.Id == id);
